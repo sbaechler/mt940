0ec9ec1ac7ab95256bc62e1fe5e748fd83c8182e
Simon Bächler <simon@stellanera.com>
2018-12-31 14:15:31 +0100

    Modify for Kantonalbank

--- README.rst
+++ README.rst
@@ -2,12 +2,11 @@
 MT940
 =========
 
-Converter from SWIFT format MT940 (Credit Suisse) to simple,
+Converter from SWIFT format MT940 (Kantonalbank) to simple,
 importable CSV format.
 
 Until now this is really a HACK but it works for me. Needed it today
-and found nothing useful on the net. If you use Xero and are a
-Credit Suisse customer this could be useful.
+and found nothing useful on the net.
 
 Maybe there will be more documentation and better code in the future.
 
--- mt940.py
+++ mt940.py
@@ -1,12 +1,42 @@
 import re
 import sys
 
-lookup = {'05': 'job number', '32': 'instruct', '33': 'benefic',
-          '24': 'conversion text', '60': 'reason', '23': 'account with',
-          '25': 'our expenses'}
+lookup = {
+    '05': 'job number',
+    '20': 'transaction reference number',
+    '21': 'related reference',
+    '23': 'account with',
+    '24': 'conversion text',
+    '25': 'account identification',
+    '28C': 'sequence number',
+    '32': 'instruct',
+    '33': 'benefic',
+    '60': 'reason',
+    '60F': 'balance before',
+    '61': 'amount',
+    '62F': 'balance after',
+    '64': 'available funds',
+    '86': 'information',
+}
 
-text = open(sys.argv[1]).read().splitlines()
-text = ''.join(text)
+
+def tag_name(tag):
+    return lookup.get(tag, tag)
+
+
+entries = []
+bookings = []
+
+titles = set()
+
+with open(sys.argv[1], 'r') as f:
+    text = f.read()
+    entries = text.split('\n\n')  # Empty line
+
+entry_re = re.compile('^:(\d+\w?):(.*)')
+
+linebreak_re = re.compile('\r\n(?!:)')
+
 
 # interesting stuff is in {4:.....}
 book_pat = re.compile('\{4:(?P<booking>.*?)\}')
@@ -15,68 +45,80 @@ book_pat = re.compile('\{4:(?P<booking>.*?)\}')
 field_pat = re.compile(':(?P<num>\d\d.??):(?P<field>[^:]*)')
 
 # seperate the values in the field 61
-val61_pat = re.compile('(?P<valuta>\d{6})(?P<date>\d{4})(?P<mark>\D\D?)'
-                       '(?P<amount>\d+,\d{2})(?P<code>\D{4})(?P<cref>.*?)//'
-                       '(?P<bref>.{16})(?P<add>.*)')
+val61_pat = re.compile('^(?P<valuta>\d{6})(?P<date>\d{4})(?P<mark>\D\D?)'
+                       '(?P<amount>\d+,\d{0,2})(?P<code>\D{4})(?P<cref>.*?)$')
 
 # seperate the values in the field 86
-value_pat = re.compile('(?P<valuta>\d{6})(?P<date>\d{4})(?P<mark>\D\D?)'
+value_pat = re.compile('^(?P<valuta>\d{6})(?P<date>\d{4})(?P<mark>\D\D?)'
                        '(?P<amount>\d+,\d{2})(?P<code>\D{4})(?P<cref>.*?)//'
-                       '(?P<bref>.{16})(?P<add>.*)')
+                       '(?P<bref>.{16})(?P<add>.*)$')
 
-f = open('export.csv', 'w')
-f.write('date, amount, payee, description, reference\n')
+balance_pat = re.compile('^C(?P<date>\d{6})(?P<currency>\w{3})(?P<amount>.*)$')
 
-for match in re.finditer(book_pat, text):
-    booking = match.group('booking')
 
-    for match in re.finditer(field_pat, booking):
-        num = match.group('num')
-        field = match.group('field')
+def get_balance(tag, entry):
+    match = balance_pat.search(entry)
+    if match:
+        return {
+            '{}:currency'.format(tag): match.group('currency'),
+            '{}:amount'.format(tag): match.group('amount').replace(',', '.'),
+        }
 
-        if num == '61':
-            match = re.match(val61_pat, field)
-            match_dict = match.groupdict()
-            print match_dict
+    return entry
 
-        if num == '86':
-            trig_dict = {}
-            triggers = field.split('?')
-            trig_dict['epc'] = triggers[0]
-            for trigger in triggers[1:]:
-                code = trigger[:2]
-                code = lookup[code]
-                value = trigger[2:]
-                trig_dict[code] = value
 
-            print trig_dict
+def parse_61(tag, entry):
+    match = val61_pat.search(entry)
+    if match:
+        return {
+            '{}:valuta'.format(tag): match.group('valuta'),
+            '{}:date'.format(tag): match.group('date'),
+            '{}:mark'.format(tag): match.group('mark'),
+            '{}:amount'.format(tag): match.group('amount').replace(',', '.'),
+        }
 
-            date = match_dict['valuta']
-            amount = match_dict['amount']
-            amount = amount.replace(',', '.')
+    return entry
 
-            if match_dict['mark'] == 'D':
-                sign = '-'
-                benefic = trig_dict['benefic']
+
+additional_processing = {
+    '60F': get_balance,
+    '61': parse_61,
+    '62F': get_balance,
+    '64': get_balance,
+}
+
+
+for entry in entries:
+    sanitized_entry = re.sub(linebreak_re, ' ', entry)
+    details = sanitized_entry.splitlines()
+    booking = {}
+
+    for row in details:
+        matches = entry_re.search(row)
+        if matches:
+            tag = matches.group(1)
+            content = matches.group(2)
+            name = tag_name(tag)
+
+            if tag in additional_processing:
+                result = additional_processing[tag](name, content)
+                if type(result) is dict:
+                    for (t, v) in result.items():
+                        titles.add(t)
+                        booking[t] = v
+                else:
+                    titles.add(name)
+                    booking[name] = result
             else:
-                sign = ''
-                benefic = trig_dict['instruct']
+                titles.add(name)
+                booking[name] = content
 
-            bref = match_dict['bref']
+    bookings.append(booking)
 
-            if 'reason' in trig_dict.keys():
-                reason = trig_dict['reason']
-            else:
-                reason = ''
 
-            amount = '{0}{1}'.format(sign, amount)
-            date = '{0}/{1}/{2}'.format(date[4:6], date[2:4], 2000 +
-                                        int(date[:2]))
-
-            f.write('{0}, {1}, {2}, {3}, {4}\n'.format(date, amount,
-                                                       benefic, reason,
-                                                       bref))
-
-            print
-
-f.close()
+with open('export.csv', 'w') as f:
+    headings = [t for t in titles]
+    f.write('; '.join(headings) + '\n')
+    for booking in bookings:
+        entry = [booking.get(h, '') for h in headings]
+        f.write('; '.join(entry) + '\n')
